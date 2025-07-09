3d3539074eb72530f9c26a8a52bb026968af6695
Kerim-Personal <batinkerimkaraman@gmail.com>
2025-07-09 18:17:14 +0300

    Update

--- app/src/main/java/com/codenzi/snapnote/CameraNoteActivity.kt
+++ app/src/main/java/com/codenzi/snapnote/CameraNoteActivity.kt
@@ -17,7 +17,6 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.codenzi.snapnote.databinding.ActivityCameraBinding
-import com.github.chrisbanes.photoview.BuildConfig
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.File
 import java.text.SimpleDateFormat
@@ -73,7 +72,7 @@ class CameraNoteActivity : AppCompatActivity() {
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
                     val savedUri = FileProvider.getUriForFile(
                         this@CameraNoteActivity,
-                        "${BuildConfig.APPLICATION_ID}.provider",
+                        "com.codenzi.snapnote.provider", // DÜZELTME: BuildConfig.APPLICATION_ID yerine doğrudan paket adı kullanıldı.
                         photoFile
                     )
                     val msg = "Photo capture succeeded: $savedUri"
