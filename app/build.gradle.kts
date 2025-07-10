9cf6f8d91c5077294f7b3d48bbe9a2aaefb06902
Kerim-Personal <batinkerimkaraman@gmail.com>
2025-07-10 04:33:41 +0300

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.codenzi.snapnote"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         // HATA BURADAYDI - SATIRLAR BİRLEŞTİRİLDİ
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
